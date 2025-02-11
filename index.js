18dc865ca0c512bf65c6d1b1f262a5ef93365c91
kaizenwho5919 <pathakarjun438@gmail.com>
2025-02-11 11:04:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const keep_alive = require('./keep_alive.js')
 
 function installModules() {
     const requiredModules = [
