263caca0a4c27572f0212c226274f890a1204e79
kaizenwho5919 <pathakarjun438@gmail.com>
2025-02-12 15:15:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const rpcStates = [
             start: Date.now()
         },
         assets: {
-            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338796205114654730/49969923d81582ef25c5ac1fedb71e45_1.gif?ex=67ac62a0&is=67ab1120&hm=de4df4f5bde05ed7673826076963cd9e119c8dae19dc068584cb740991ff8b74&=',
+            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338796205114654730/49969923d81582ef25c5ac1fedb71e45_1.gif?ex=67adb420&is=67ac62a0&hm=7e5415b82718b65fd8017ee32b19f8bd793bf12e4086f4ba14fab8aab14fb846&=',
             largeText: 'CR7🗿..!?',
             smallImage: 'https://cdn.discordapp.com/emojis/1224756226311852042.gif?size=48',
             smallText: 'Hacker Mode'
@@ -78,13 +78,13 @@ const rpcStates = [
     {
         type: 'WATCHING',
         name: '! Mxbye Arjun 🥀',
-        details: '! Mxbye Unrivaled..!? 🥀',
-        state: 'Pookiee..!?',
+        details: '! Unrivaled..!? 🥀',
+        state: 'Typee shii..!?',
         timestamps: {
             start: Date.now()
         },
         assets: {
-            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338803197086666793/ea506e71dd3c834f10805275cd65a116.jpg?ex=67ac6923&is=67ab17a3&hm=c1607b8d1c5dd544300a77afc2594caafa7b2d586befc0b1179814ffc85327af&=&format=webp&width=391&height=631',
+            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338795355659046922/4eb056dae9372fb0907723a060fd3f84.gif?ex=67adb356&is=67ac61d6&hm=d876e83c646663ebbcf7166442c84bbdc71e369e0fca042fa2f9e4e811e09174&=',
             largeText: 'Nym..!?',
             smallImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338126456705777746/8285d845fbce5ede830d6291fec56b14.gif?ex=67ad3ea0&is=67abed20&hm=e7ffeab881385bc7ffc6d6779791828b94ae8b33a3f1c500fa189328250f1a95&=',
             smallText: 'Damn..!?'
@@ -103,7 +103,7 @@ const rpcStates = [
             start: Date.now()
         },
         assets: {
-            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338920892809347163/20220827_072402.gif?ex=67acd6c0&is=67ab8540&hm=3ff646acf0c24d78c0de999dd5a20c18786d2cff9db30c1ff36b9fc0012d7b5c&=',
+            largeImage: 'https://media.discordapp.net/attachments/1277534645633683469/1338920892809347163/20220827_072402.gif?ex=67ad7f80&is=67ac2e00&hm=c6004393f94e2802ea813b32e6ac1f43829bcde44330993dee0734af9e409514&=',
             largeText: 'Hihi...',
             smallImage: 'https://cdn.discordapp.com/emojis/1312633052668629055.gif?size=48',
             smallText: 'Be aware'
